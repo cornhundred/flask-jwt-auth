f69151595af83d975cb4e030c2b2355b2b34bea9
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-09-11 12:54:36 -0400

    fixed previous test definitions, working on registered user login test

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -56,29 +56,69 @@ class TestUserModel(BaseTestCase):
       self.assertTrue(response.content_type == 'application/json')
       self.assertTrue(response.status_code, 201)
 
-def test_registered_with_already_registered_user(self):
-    """ Test registration with already registered email"""
-    user = User(
-      email='joe@gmail.com',
-      password='test'
-    )
-    db.session.add(user)
-    db.session.commit()
-    with self.client:
-      response = self.client.post(
-        '/auth/register',
-        data=json.dumps(dict(
-          email='joe@gmail.com',
-          password='123456'
-        )),
-        content_type='application/json'
+  def test_registered_with_already_registered_user(self):
+      """ Test registration with already registered email"""
+      user = User(
+        email='joe@gmail.com',
+        password='test'
       )
-      data = json.loads(response.data.decode())
-      self.assertTrue(data['status'] == 'fail')
-      self.assertTrue(
-        data['message'] == 'User already exists. Please Log in.')
-      self.assertTrue(response.content_type == 'application/json')
-      self.assertEqual(response.status_code, 202)
+      db.session.add(user)
+      db.session.commit()
+      with self.client:
+        response = self.client.post(
+          '/auth/register',
+          data=json.dumps(dict(
+            email='joe@gmail.com',
+            password='123456'
+          )),
+          content_type='application/json'
+        )
+        data = json.loads(response.data.decode())
+        self.assertTrue(data['status'] == 'fail')
+        self.assertTrue(
+          data['message'] == 'User already exists. Please Log in.')
+        self.assertTrue(response.content_type == 'application/json')
+        self.assertEqual(response.status_code, 202)
+
+  # def test_registered_user_login(self):
+  #   """ Test for login of registered-user login """
+  #   print('HERE')
+  #   with self.client:
+  #     # user registration
+  #     resp_register = self.client.post(
+  #       '/auth/register',
+  #       data = json.dumps(dict(
+  #         email = 'joe@gmail.com',
+  #         password = '123456'
+  #       )),
+  #       content_type = 'application/json',
+  #     )
+
+  #     data_register = json.loads(resp_register.data.decode())
+
+  #     self.assertTrue(data_register['status'] == 'success')
+  #     self.assertTrue(
+  #       data_register['message'] == 'Successfully registered.'
+  #     )
+  #     self.assertTrue(data_register['auth_token'])
+  #     self.assertTrue(resp_register.content_type == 'application/json')
+  #     self.assertEqual(resp_register.status_code, 201)
+
+  #     # registered user login
+  #     response = self.client.post(
+  #       '/auth/login',
+  #       data = json.dumps(dict(
+  #         email = 'joe@gmail.com',
+  #         password = '123456'
+  #       )),
+  #       content_type = 'application_json'
+  #     )
+  #     data = json.loads(response.data.decode())
+  #     self.assertTrue(data['status'] == 'success')
+  #     self.assertTrue(data['message'] == 'Successfully logged in.')
+  #     self.assertTrue(data['auth_token'])
+  #     self.assertTrue(response.content_type == 'application/json')
+  #     self.assertEqual(response.status_code, 200)
 
 if __name__ == '__main__':
   unittest.main()
