18d4b38404f04b73a7a90b9a34e8b6e886441540
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-09-11 12:23:42 -0400

    added test for registration with user that already exists

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -10,9 +10,10 @@ from project.tests.base import BaseTestCase
 class TestUserModel(BaseTestCase):
 
   def test_encode_auth_token(self):
-    '''
+    """
     Test encode auth token
-    '''
+    """
+
     inst_email = 'test@test.com'
     user = User(
         email = inst_email,
@@ -24,9 +25,9 @@ class TestUserModel(BaseTestCase):
     self.assertTrue(isinstance(auth_token, bytes))
 
   def test_decode_auth_token(self):
-    '''
+    """
     Test Decode auth token
-    '''
+    """
     user = User(
       email = 'test@test.com',
       password = 'test'
@@ -54,6 +55,30 @@ class TestUserModel(BaseTestCase):
       self.assertTrue(data['auth_token'])
       self.assertTrue(response.content_type == 'application/json')
       self.assertTrue(response.status_code, 201)
+
+def test_registered_with_already_registered_user(self):
+    """ Test registration with already registered email"""
+    user = User(
+      email='joe@gmail.com',
+      password='test'
+    )
+    db.session.add(user)
+    db.session.commit()
+    with self.client:
+      response = self.client.post(
+        '/auth/register',
+        data=json.dumps(dict(
+          email='joe@gmail.com',
+          password='123456'
+        )),
+        content_type='application/json'
+      )
+      data = json.loads(response.data.decode())
+      self.assertTrue(data['status'] == 'fail')
+      self.assertTrue(
+        data['message'] == 'User already exists. Please Log in.')
+      self.assertTrue(response.content_type == 'application/json')
+      self.assertEqual(response.status_code, 202)
 
 if __name__ == '__main__':
   unittest.main()
