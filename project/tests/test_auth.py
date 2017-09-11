5af563d092ffa95b2d78495a2694ab85d413916b
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-09-11 12:01:25 -0400

    getting the right error

---
+++ project/tests/test_auth.py
@@ -0,0 +1,15 @@
+# project/tests/auth_test.py
+# holds tests for the auth_blueprint
+
+import unittest
+
+from project.server import db
+from project.server.models import User
+from project.tests.base import BaseTestCase
+
+class TestAuthBlueprint(BaseTestCase):
+  pass
+
+if __name__ == '__main__':
+  unittest.main()
+
--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -1,6 +1,7 @@
 # project/tests/test_user_model.py
 
 import unittest
+import json
 
 from project.server import db
 from project.server.models import User
@@ -29,6 +30,24 @@ class TestUserModel(BaseTestCase):
     auth_token = user.encode_auth_token(user.id)
     self.assertTrue(isinstance(auth_token, bytes))
     self.assertTrue(User.decode_auth_token(auth_token) == 1)
+
+  def test_registrtion(self):
+    """ Test for user registration """
+    with self.client:
+      response = self.client.post(
+        'auth/register',
+        data = json.dumps(dict(
+          email = 'joe@gmail.com',
+          password = '123456'
+        )),
+        content_type = 'application/json'
+      )
+      data = json.loads(response.data.decode())
+      self.assertTrue(data['status'] == 'success')
+      self.assertTrue(data['message'] == 'Successfully registered.')
+      self.assertTrue(data['auth_token'])
+      self.assertTrue(response.content_type == 'application/json')
+      self.assertTrue(response.status_code, 201)
 
 if __name__ == '__main__':
   unittest.main()
