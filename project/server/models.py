10db74c42b92351c356c66a4768e67d46687929c
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-09-13 14:22:40 -0400

    working on adding blacklist token table to database

--- project/server/models.py
+++ project/server/models.py
@@ -154,3 +154,20 @@ class UserAPI(MethodView):
         'message': 'Provide a vaild auth token'
       }
       return make_response(jsonify(responseObject)), 401
+
+class BlacklistToken(db.Model):
+  """
+  Token Model for storing JWT tokens
+  """
+  __table_name__ = 'blacklist_tokens'
+
+  id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+  token = db.Column(db.String(500), unique=True, nullable=False)
+  blacklisted_on = db.Column(db.DateTime, nullable=False)
+
+  def __init__(self, token):
+    self.token = token
+    self.blacklisted_on = datetime.datetime.now()
+
+  def __repr__(self):
+    return '<id: token: {}'.format(self.token)
