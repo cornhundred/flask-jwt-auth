f58d43822fdcbcaf9a52fe50e596ae54171ce7d2
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-09-13 14:45:07 -0400

    13 tests working, test_valid_blacklisted_token_logout

--- project/tests/test_user_model.py
+++ project/tests/test_user_model.py
@@ -4,7 +4,7 @@ import unittest
 import json
 
 from project.server import db
-from project.server.models import User
+from project.server.models import User, BlacklistToken
 from project.tests.base import BaseTestCase
 import time
 
@@ -29,18 +29,17 @@ class TestUserModel(BaseTestCase):
     self.assertTrue(isinstance(auth_token, bytes))
 
   def test_decode_auth_token(self):
-    """
-    Test Decode auth token
-    """
     user = User(
-      email = 'test@test.com',
-      password = 'test'
+      email='test@test.com',
+      password='test'
     )
     db.session.add(user)
     db.session.commit()
     auth_token = user.encode_auth_token(user.id)
     self.assertTrue(isinstance(auth_token, bytes))
-    self.assertTrue(User.decode_auth_token(auth_token) == 1)
+    self.assertTrue(User.decode_auth_token(
+        auth_token.decode("utf-8") ) == 1
+    )
 
   def test_registrtion(self):
     """ Test for user registration """
@@ -288,6 +287,62 @@ class TestUserModel(BaseTestCase):
         data['message'] == 'Signature expired. Please log in again.'
       )
       self.assertEqual(response.status_code, 401)
+
+
+  def test_valid_blacklisted_token_logout(self):
+    """ Test for logout after a valid token gets blacklisted """
+    with self.client:
+      # user registration
+      resp_register = self.client.post(
+        '/auth/register',
+        data=json.dumps(dict(
+          email='joe@gmail.com',
+          password='123456'
+        )),
+        content_type='application/json',
+      )
+      data_register = json.loads(resp_register.data.decode())
+      self.assertTrue(data_register['status'] == 'success')
+      self.assertTrue(
+        data_register['message'] == 'Successfully registered.')
+      self.assertTrue(data_register['auth_token'])
+      self.assertTrue(resp_register.content_type == 'application/json')
+      self.assertEqual(resp_register.status_code, 201)
+      # user login
+      resp_login = self.client.post(
+        '/auth/login',
+        data=json.dumps(dict(
+          email='joe@gmail.com',
+          password='123456'
+        )),
+        content_type='application/json'
+      )
+      data_login = json.loads(resp_login.data.decode())
+      self.assertTrue(data_login['status'] == 'success')
+      self.assertTrue(data_login['message'] == 'Successfully logged in.')
+      self.assertTrue(data_login['auth_token'])
+      self.assertTrue(resp_login.content_type == 'application/json')
+      self.assertEqual(resp_login.status_code, 200)
+      # blacklist a valid token
+      blacklist_token = BlacklistToken(
+        token=json.loads(resp_login.data.decode())['auth_token'])
+      db.session.add(blacklist_token)
+      db.session.commit()
+      # blacklisted valid token logout
+      response = self.client.post(
+        '/auth/logout',
+        headers=dict(
+          Authorization='Bearer ' + json.loads(
+            resp_login.data.decode()
+          )['auth_token']
+        )
+      )
+      data = json.loads(response.data.decode())
+      self.assertTrue(data['status'] == 'fail')
+      self.assertTrue(data['message'] == 'Token blacklisted. Please log in again.')
+      self.assertEqual(response.status_code, 401)
+
+
 
 
 if __name__ == '__main__':
